b0e9498c0f9abadfee40f67509f980e1fb51a608
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2020-09-20 13:59:52 +0530

    setting up and using context

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import githubContext from "./githubContext";
+import githubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_REPOS,
+  GET_USER,
+} from "../types";
+
+const GithubState = (props) => {
+  const initalState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initalState);
+
+  return (
+    <githubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </githubContext.Provider>
+  );
+};
+
+export default GithubState;
